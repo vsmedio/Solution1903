1c5f24cff4a3b184b55de1f0911d6362198180c7
Arvis Abromovics <info@vsmedio.lv>
2018-01-11 21:52:56 +0100

    Atrisināts!!!

--- src/Solution.java
+++ src/Solution.java
@@ -61,8 +61,12 @@ public class Solution {
 }
 */
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
     public static Map<String, String> countries = new HashMap<String, String>();
+
     static {
         countries.put("UA", "Ukraine");
         countries.put("RU", "Russia");
@@ -70,9 +74,7 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-
     }
-
 
     public static class IncomeDataAdapter implements Customer, Contact {
         private IncomeData data;
@@ -87,6 +89,17 @@ public class Solution {
         }
 
         @Override
+        public String getPhoneNumber() {
+            String full = String.format("%010d", data.getPhoneNumber());
+            String telPart1 = "+" + data.getCountryPhoneCode();
+            String telPart2 = full.substring(0, 3);
+            String telPart3 = full.substring(3, 6);
+            String telPart4 = full.substring(6, 8);
+            String telPart5 = full.substring(8, 10);
+            return String.format("%s(%s)%s-%s-%s", telPart1, telPart2, telPart3, telPart4, telPart5);
+        }
+
+        @Override
         public String getCompanyName() {
             return data.getCompany();
         }
@@ -95,44 +108,24 @@ public class Solution {
         public String getCountryName() {
             return countries.get(data.getCountryCode());
         }
-
-        @Override
-        public String getPhoneNumber() {
-            String full = String.format("%010d", data.getPhoneNumber());
-            String telPart1 = "+" + data.getCountryPhoneCode();
-            String telPart2 = full.substring(0, 3);
-            String telPart3 = full.substring(3, 6);
-            String telPart4 = full.substring(6, 8);
-            String telPart5 = full.substring(8, 10);
-            String telFormated = String.format("%s(%s)%s-%s-%s", telPart1, telPart2, telPart3, telPart4, telPart5);
-            return telFormated;
-        }
     }
-
 
     public static interface IncomeData {
         String getCountryCode();        //example UA
-
         String getCompany();            //example JavaRush Ltd.
-
         String getContactFirstName();   //example Ivan
-
         String getContactLastName();    //example Ivanov
-
         int getCountryPhoneCode();      //example 38
-
         int getPhoneNumber();           //example 501234567
     }
 
     public static interface Customer {
         String getCompanyName();        //example JavaRush Ltd.
-
         String getCountryName();        //example Ukraine
     }
 
     public static interface Contact {
         String getName();               //example Ivanov, Ivan
-
         String getPhoneNumber();        //example +38(050)123-45-67
     }
 }
